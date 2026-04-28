91c893bed652f3da54fe81620567395ec08cfe3e
rivafarabi <rivafarabi.babeheer@gmail.com>
2026-04-28 23:59:32 +0700

    make UserLink's portfolio into nullable

--- lib/src/model/user.dart
+++ lib/src/model/user.dart
@@ -197,7 +197,7 @@ class UserLinks extends ModelBase {
     required this.html,
     required this.photos,
     required this.likes,
-    required this.portfolio,
+    this.portfolio,
     this.followers,
     this.following,
   }) : super(source: source);
@@ -206,7 +206,7 @@ class UserLinks extends ModelBase {
   final Uri html;
   final Uri photos;
   final Uri likes;
-  final Uri portfolio;
+  final Uri? portfolio;
   final Uri? followers;
   final Uri? following;
 
@@ -217,7 +217,7 @@ class UserLinks extends ModelBase {
       'html': html.toString(),
       'photos': photos.toString(),
       'likes': likes.toString(),
-      'portfolio': portfolio.toString(),
+      'portfolio': portfolio?.toString(),
       'followers': followers?.toString(),
       'following': following?.toString(),
     };
@@ -230,7 +230,7 @@ class UserLinks extends ModelBase {
       html: (json['html'] as String).let(Uri.parse),
       photos: (json['photos'] as String).let(Uri.parse),
       likes: (json['likes'] as String).let(Uri.parse),
-      portfolio: (json['portfolio'] as String).let(Uri.parse),
+      portfolio: (json['portfolio'] as String?)?.let(Uri.parse),
       followers: (json['followers'] as String?)?.let(Uri.parse),
       following: (json['following'] as String?)?.let(Uri.parse),
     );
